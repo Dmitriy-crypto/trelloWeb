85bbf6abed9d228bbea927beca60cc11abf91225
Dmitriy <dmitriystadnikov17@gmail.com>
2019-12-03 11:37:27 +0200

    6_$start create new methods in @Test

--- trelloWeb/src/test/java/com/trello/qa/TeamCreationTests.java
+++ trelloWeb/src/test/java/com/trello/qa/TeamCreationTests.java
@@ -1,12 +1,17 @@
 package com.trello.qa;
 //one upload start project
-import org.openqa.selenium.By;
+
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class TeamCreationTests extends TestBase {
     @Test
-    public void TeamCreation() {
+    public void testTeamCreationFromButtonOnHeader() {
+
+        clickOnPlusButtonOnHeader();
+        selestCreateTeamFromDropDoun();
+        fillTeamCreationForm();
+        clickContinueButtun();
         Assert.assertTrue(isUserLoggedIn(), "ass");
 
     }
