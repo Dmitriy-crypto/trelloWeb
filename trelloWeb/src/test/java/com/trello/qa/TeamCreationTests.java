aabbe87ac5e171785dae449345755f5dc1b84e21
Dmitriy <dmitriystadnikov17@gmail.com>
2019-12-05 13:32:12 +0200

    8_Added classes to board and team creation

--- trelloWeb/src/test/java/com/trello/qa/TeamCreationTests.java
+++ trelloWeb/src/test/java/com/trello/qa/TeamCreationTests.java
@@ -1,39 +1,33 @@
 package com.trello.qa;
 //one upload start project
 
-import org.openqa.selenium.By;
+
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class TeamCreationTests extends TestBase {
-    @Test
+
+
+    @Test(enabled = true)
     public void testTeamCreationFromButtonOnHeader() {
-        String teamName = "Dmitriy_qa_learn";
-        String description = "descr Learn";
+
         clickOnPlusButtonOnHeader();
-        selestCreateTeamFromDropDoun();
-        fillTeamCreationForm("s", "");
-        clickContinueButtun();
+        selectCreateTeamFromDropDon();
+        fillTeamCreationForm(teamName, description);
+        clickContinueButton();
         Assert.assertTrue(isUserLoggedIn(), "ass");
 
     }
 
-    public void clickContinueButtun() {
-        click(By.cssSelector("[type=submit]"));
-    }
+    @Test(enabled = false)
+    public void testTeamCancelCreationFromButtonOnHeader() {
 
-    public void fillTeamCreationForm(String teamName, String description) {
-        type(By.cssSelector("[data-test-id='header-create-team-name-input']"), teamName);
-        type(By.cssSelector("textarea"), description);
-    }
+        clickOnPlusButtonOnHeader();
+        selectCreateTeamFromDropDon();
+        fillTeamCreationForm(teamName, description);
+        clickXorCancel();
+        Assert.assertTrue(isUserLoggedIn(), "ass");
 
-    public void selestCreateTeamFromDropDoun() {
-        click(By.cssSelector("[data-test-id='header-create-team-button']"));
-
-    }
-
-    public void clickOnPlusButtonOnHeader() {
-        click(By.cssSelector("[data-test-id='header-create-menu-button']"));
     }
 
 
--- trelloWeb/src/test/java/com/trello/qa/TestBase.java
+++ trelloWeb/src/test/java/com/trello/qa/TestBase.java
@@ -11,14 +11,26 @@ import java.util.concurrent.TimeUnit;
 public class TestBase {
 
     WebDriver driver;
+    //-------------------Variables-------------------------------------
 
     public static String url = "https://trello.com/";
+
+    //-------------------login ahd password-----------------------------
     public static String email = "elena.telran@yahoo.com";
     public static String password = "12345.com";
 
-    
+
     //public static String email = "dmitriy.stadnikov@yahoo.com";
     //public static String password = "75dmitriy.13Eva";
+
+    //------------------Variables for teams fill----------------------------------------
+    public String teamName = "test_can_be_deleted";
+    public String description = "descr Learn_delete_ok";
+
+    //----------------------------Variables for boards fill------------------------------------------------------
+    public String boardName = "board off test, can_be_deleted";
+
+    //-------------------End variables-------------------------------------------
 
     @BeforeMethod
     public void setUp() {
@@ -27,39 +39,12 @@ public class TestBase {
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         driver.manage().window().maximize();
 
-//driver.manage().window().fullscreen();
+        //driver.manage().window().fullscreen();
 
         openSite(url);
-        login(email, password);
+        logInToTrello(email, password);
 
 
-    }
-
-    public void login(String email, String password) {
-        //click(By.cssSelector("[href='/login]'"));
-        click(By.cssSelector("[href='/login']"));
-        type(By.cssSelector("[type=email]"), email);
-        type(By.cssSelector("[type=password]"), password);
-        click(By.id("login"));
-
-    }
-
-    public void click(By locator) {
-        driver.findElement(locator).click();
-    }
-
-
-    public void type(By locator, String text) {
-        driver.findElement(locator).click();
-        driver.findElement(locator).clear();
-        driver.findElement(locator).sendKeys(text);
-
-
-    }
-
-    //----------------------------------------------------------------------------
-    public void openSite(String url) {
-        driver.navigate().to(url);
     }
 
     @AfterMethod
@@ -71,8 +56,69 @@ public class TestBase {
         return isElementPresent(By.cssSelector("[data-test-id='header-member-menu-button']"));
     }
 
-    private boolean isElementPresent(By locator) {
+    public boolean isElementPresent(By locator) {
         return driver.findElements(locator).size() > 0;
 
+    }
+
+    //------------------Methods---------------------------------------------------
+
+    public void logInToTrello(String email, String password) {
+        clickOnWebElement(By.cssSelector("[href='/login']"));
+        typeTextInTheField(By.cssSelector("[type=email]"), email);
+        typeTextInTheField(By.cssSelector("[type=password]"), password);
+        clickOnWebElement(By.id("login"));
+    }
+
+    public void clickOnWebElement(By locator) {
+        driver.findElement(locator).click();
+    }
+
+    public void typeTextInTheField(By locator, String text) {
+        driver.findElement(locator).click();
+        driver.findElement(locator).clear();
+        driver.findElement(locator).sendKeys(text);
+    }
+
+    public void openSite(String url) {
+        driver.navigate().to(url);
+    }
+
+    public void clickContinueButton() {
+        clickOnWebElement(By.cssSelector("[type=submit]"));
+    }
+
+    public void fillTeamCreationForm(String teamName, String description) {
+        typeTextInTheField(By.cssSelector("[data-test-id='header-create-team-name-input']"), teamName);
+        typeTextInTheField(By.cssSelector("textarea"), description);
+    }
+
+    public void selectCreateTeamFromDropDon() {
+        clickOnWebElement(By.cssSelector("[data-test-id='header-create-team-button']"));
+
+    }
+
+    public void clickOnPlusButtonOnHeader() {
+        clickOnWebElement(By.cssSelector("[data-test-id='header-create-menu-button']"));
+    }
+
+    public void clickXorCancel() {
+    }
+
+    public void selectCreateBoardFromDropDown() {
+        clickOnWebElement(By.cssSelector("[data-test-id='header-create-board-button']"));
+    }
+
+    public void fillBoardCreationForm(String boardName, String description) {
+
+        typeTextInTheField(By.cssSelector("[data-test-id='create-board-title-input']"), boardName);
+        if (isElementPresent(By.cssSelector(".W6rMLOx8U0MrPx"))) {
+            clickOnWebElement(By.cssSelector(".W6rMLOx8U0MrPx"));
+            clickOnWebElement(By.xpath("//nav[@class='SdlcRrTVPA8Y3K']//li[1]"));//no team
+        }
+    }
+
+    public void confirmBoardCreation() {
+        clickOnWebElement(By.cssSelector("[data-test-id='create-board-submit-button']"));
     }
 }
