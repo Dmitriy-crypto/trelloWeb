46b4d3e5dec17a49eb0a6fb04f9650c77dff03e3
Dmitriy-crypto <dmitriystadnikov17@gmail.com>
2020-05-27 19:58:01 +0300

    1

--- trelloWeb/src/test/java/com/trello/qa/tests/TeamDeletionTests.java
+++ trelloWeb/src/test/java/com/trello/qa/tests/TeamDeletionTests.java
@@ -11,22 +11,25 @@ public class TeamDeletionTests extends TestBase {
 
         int i;//count delete boards
         int before = app.getTeamHelper().getTeamsCount();
-        System.out.println(before);
+        System.out.println(before+" before For");
+        if(!(before ==0)){
         for (i = 0; (before) >= 1; before--, i++) {
             //for (i = 0; (before + 1) > 1; before--, i++) {
+            System.out.println(before+" for ");
             app.getTeamHelper().clickOnFirstTeam();
             app.getTeamHelper().openSettings();
             app.getTeamHelper().deleteTeam();
         }
         System.out.println("number deleted  " + i);
-        app.getSessionHelper().refreshPage();
-        int after = app.getTeamHelper().getTeamsCount();
-        System.out.println(after);
+
 /*if(before==0){
     before=1;
-}*/
+}*/}  app.getSessionHelper().refreshPage();
+        app.getTeamHelper().after = app.getTeamHelper().getTeamsCount();
+        System.out.println(before+" after For");
+        System.out.println("after "+app.getTeamHelper().after);
 
-        Assert.assertEquals(after, before);
+        Assert.assertEquals(app.getTeamHelper().after, before);
     }
 
 
